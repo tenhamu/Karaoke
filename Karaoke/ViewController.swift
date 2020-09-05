f062cb7bb4d14d4e7d6065e164a95fd8d7f2d95d
takumayuusuke <sfayfayc@i.softbank.jp>
2020-09-05 15:35:25 +0900

    Initial Commit

---
+++ Karaoke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Karaoke
+//
+//  Created by 卓馬優介 on 2020/09/05.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KaraokeTests/KaraokeTests.swift
@@ -0,0 +1,34 @@
+//
+//  KaraokeTests.swift
+//  KaraokeTests
+//
+//  Created by 卓馬優介 on 2020/09/05.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import XCTest
+@testable import Karaoke
+
+class KaraokeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
